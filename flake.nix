c78947620efd16b4bacf133087ea56e2051499c9
arzumify <arzumify@disroot.org>
2026-09-16 00:10:51 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  imports = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  
+  outputs = {nixpkgs}: let
+    lib = nixpkgs.lib;
+    forAllSystems = f: lib.genAttrs lib.systems.flakeExposed (system: f {
+      pkgs = nixpkgs.legacyPackages.${system};
+    });
+  in {
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          gopls
+        ];
+      };
+    });
+  };
+}
