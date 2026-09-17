4ec7e126d1993b04db286ae848401f4ce68ac9e0
arzumify <arzumify@disroot.org>
2026-09-18 00:21:33 +0100

    cd into tests then run tests

--- pritty_test.go
+++ pritty_test.go
@@ -2,5 +2,5 @@ package pritty
 
 func init() {
 	println("!!! Go unit testing does not work correctly for raw terminal input. !!!")
-	println("!!! Use \"go run tests/tests.go\" instead! !!!\n")
+	println("!!! Use \"cd tests && go run tests.go\" instead! !!!\n")
 }
